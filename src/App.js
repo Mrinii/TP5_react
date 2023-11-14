6972b898391a81b6057deb0a6eb750947acef912
Mrinii <melmrini02@gmail.com>
2023-11-14 15:44:38 +0100

    message

--- src/App.js
+++ src/App.js
@@ -1,40 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
 import React from 'react';
-import {Routes,Route} from 'react-router-dom';
+import { Routes,Route } from 'react-router-dom';
 import Tp5 from './Tp5';
-import CalcState from  './CalcState';
+import CalcState from './CalcState';
 import Slider from './Slider';
 import Formtasks from './Formtasks';
 import UseEffectTp from './UseEffectTp';
 
-
-
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <Tp5/>
-      <Routes>
-        <Route path='/TP1_react' element={<CalcState/>}/>
-        <Route path='/Slider' element={<Slider/>}/>
-        <Route path='/Formtasks' element={<Formtasks/>}/>
-        <Route path='/UseEffectTp' element={<UseEffectTp/>}/>
-      </Routes>
-    </div>
+    
+      <div className="App">
+        <Tp5 />
+        <Routes>
+          <Route path="/CalcState" element={<CalcState />} />
+          <Route path="/Slider" element={<Slider />} />
+          <Route path="/Formtasks" element={<Formtasks />} />
+          <Route path="/UseEffectTp" element={<UseEffectTp />} />
+        </Routes>
+      </div>
+    
   );
 }
 
--- src/Tp5.js
+++ src/Tp5.js
@@ -1,33 +1,28 @@
-import {React} from 'react';
-import { Link } from 'react-router-dom' ;
+// Tp5.js
+import React from 'react';
+import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import CalcState from  './CalcState';
-import Slider from './Slider';
-import Formtasks from './Formtasks';
-import UseEffectTp from './UseEffectTp';
 
 export default function Tp5() {
-return (
-    <div >
-    <Navbar expand="lg" className="bg-body-tertiary">
-    <Container>
-      <Navbar.Brand href="#home">NavBArLinks</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link href="/TP1_react">Calculatrice</Nav.Link>
-          <Nav.Link href="/Slider">Slider</Nav.Link>
-          <Nav.Link href="/Formtasks">To Do Tasks</Nav.Link>
-          <Nav.Link href="/UseEffectTp">API</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
+  return (
+    <div>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">NavBArLinks</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/CalcState">Calculatrice</Nav.Link>
+              <Nav.Link as={Link} to="/Slider">Slider</Nav.Link>
+              <Nav.Link as={Link} to="/Formtasks">To Do Tasks</Nav.Link>
+              <Nav.Link as={Link} to="/UseEffectTp">API</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
-)
+  );
 }
-
-
